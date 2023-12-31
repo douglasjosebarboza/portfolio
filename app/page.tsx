173f3653a7349540a60353cc617a33eb942f1667
Douglas José Barboza <douglasbarbozajm@gmail.com>
2023-12-31 17:17:15 -0300

    user basic infos

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+import UserCard from '@/components/UserCard'
 export default function Home() {
   return (
     <main className="mx-auto my-40 grid w-full max-w-7xl gap-12 px-6">
-      <section>user</section>
+      <UserCard />
       <section>about</section>
       <section className="col-span-2">projects</section>
     </main>
---
+++ components/UserCard.jsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+
+export default function UserCard() {
+  return (
+    <section className="flex flex-col items-center gap-y-3 py-0 pb-10 pt-0">
+      <picture className="shadow-image mt-[-104px] h-52 w-52 rounded-xl">
+        <Image
+          className="h-full w-full rounded-xl object-cover"
+          src="/profile.jpg"
+          width={500}
+          height={500}
+          alt="Picture of the author"
+        />
+      </picture>
+      <div className="flex flex-col items-center gap-y-2">
+        <h1 className="text-3xl font-bold">Douglas José Barboza</h1>
+        <p className="text-lg font-light">Desenvolvedor Frontend</p>
+      </div>
+    </section>
+  )
+}
