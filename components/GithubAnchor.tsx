a7df9927d3bdfdf81fc380101245a7b16517f1a2
Douglas José Barboza <douglasbarbozajm@gmail.com>
2024-01-03 16:48:35 -0300

    acessibilidade

--- components/GithubAnchor.tsx
+++ components/GithubAnchor.tsx
@@ -6,8 +6,9 @@ export default function GithubAnchor() {
     <a
       className="flex h-12 w-12 items-center justify-center
       bg-[var(--background-body)] text-[var(--secondary)] transition-all hover:translate-y-[-12px] hover:bg-[var(--link)] hover:text-white"
-      href="#"
+      href="https://github.com/douglasjosebarboza"
       target="_blank"
+      aria-label="Link para o GitHub do Douglas José Barboza"
     >
       <FontAwesomeIcon className="h-6 w-6" icon={faSquareGithub} />
     </a>
--- components/InstagramAnchor.tsx
+++ components/InstagramAnchor.tsx
@@ -6,8 +6,9 @@ export default function InstagramAnchor() {
     <a
       className="flex h-12 w-12 items-center justify-center
       bg-[var(--background-body)] text-[var(--secondary)] transition-all hover:translate-y-[-12px] hover:bg-[var(--link)] hover:text-white"
-      href="#"
+      href="https://www.instagram.com/silenty.jb/"
       target="_blank"
+      aria-label="Link para o Instagram do Douglas José Barboza"
     >
       <FontAwesomeIcon className="h-6 w-6" icon={faInstagram} />
     </a>
